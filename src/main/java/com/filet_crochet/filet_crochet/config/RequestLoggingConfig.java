77c06da7b82123e1bdce58a47040b732e8d75887
Servando <swyswy214@gmail.com>
2026-06-30 20:39:26 -0500

    Add request logging configuration and update logging level in application properties

---
+++ src/main/java/com/filet_crochet/filet_crochet/config/RequestLoggingConfig.java
@@ -0,0 +1,17 @@
+package com.filet_crochet.filet_crochet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+@Configuration
+public class RequestLoggingConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
+}
